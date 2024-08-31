bcd9938e2c441802015c433fa6d1d94abceee31d
Emanuel Jovito <emanuel.jovito@ceuma.br>
2024-08-31 19:26:35 -0300

    feat: Estrutura inicial

---
+++ src/GlobalStyled.ts
@@ -0,0 +1,17 @@
+'use client';
+
+import { createGlobalStyle } from 'styled-components';
+
+export const Global = createGlobalStyle`
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background-color: ${({ theme }) => theme.colors.body};
+    color: black;
+  }
+`;
