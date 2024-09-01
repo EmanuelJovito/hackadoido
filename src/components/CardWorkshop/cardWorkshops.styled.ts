4ab12c8f49315990ec25983ea8b5d485969d6932
Emanuel Jovito <emanuel.jovito@ceuma.br>
2024-09-01 16:03:49 -0300

    alterei sessão oficinas

--- src/components/CardWorkshop/cardWorkshops.styled.ts
+++ src/components/CardWorkshop/cardWorkshops.styled.ts
@@ -2,12 +2,74 @@ import { styled } from "styled-components";
 import Image from "next/image";
 
 export const cardContent = styled.div`
-  /* background-color: #FFF; */
   width: 384px;
   height: 500px;
   border-radius: 24px;
+
 `
 
 export const cardImage = styled(Image)`
+  position: absolute;
 
 `;
+
+export const cardButton = styled.button`
+  width: 140px;
+  height: 50px; 
+  display: flex;
+  justify-content: center;
+  gap: 4px;
+  align-items: center;
+  background-color: #0841D3;
+  border-radius: 42px;
+  border: none;
+  padding: 10px;
+  color: white;
+  font-size: 16px;
+
+`;
+
+export const cardTitle = styled.h1`
+  color: #0841D3;
+  font-size: 32px;
+  font-weight: 600;
+  position: relative;
+  bottom: 0;
+`
+
+export const CardDescription = styled.h2`
+  color: #20241E;
+  font-size: 16px;
+  font-weight: 400;
+  position: relative;
+  opacity: 0.8;
+`;
+
+export const cardInfosContent = styled.div`
+  height: 100%;
+  position: relative;
+  z-index: 1;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 2rem;
+  width: 100%;
+`
+
+export const Infos = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+export const buttonContent = styled.div`
+  display: flex;
+  justify-content: flex-end;
+ 
+`
+export const descriptionContent = styled.div`
+  display: flex;
+  gap: 0.5rem;
+ 
+`
--- src/presentation/modules/WorkshopsSection/WorkshopsSection.tsx
+++ src/presentation/modules/WorkshopsSection/WorkshopsSection.tsx
@@ -5,22 +5,25 @@ import  { CardWorkshop }  from "../../../components/CardWorkshop/cardWorkshops"
 import  oficina1  from "@/../public/oficina1.png"
 import  oficina2  from "@/../public/oficina2.png"
 import  oficina3  from "@/../public/oficina3.png"
+import background from "../../../../public/section-timeline.png"
 
 
 export function WorkshopsSection(){
   return(
     <S.Main>
-          <Wrapper>
+      {/* <S.backgroundImg src={background} alt="background" /> */}
+         <Wrapper>
         <S.WhorksContent>
+          <S.Title>Oficinas de Inovação</S.Title>
              <S.CardContent>
-                <CardWorkshop src={oficina1}/>
-                <CardWorkshop src={oficina2}/>
-                <CardWorkshop src={oficina3}/>
+                <CardWorkshop src={oficina1} title="Oficina de Robôtica" description="Estação Tech - João de Deus"/>
+                <CardWorkshop src={oficina2} title="Oficina de Manicure" description="Estação Tech - Bacanga"/>
+                <CardWorkshop src={oficina3} title="Oficina de Culinária" description="Estação Tech - Sol e Mar"/>
             </S.CardContent>
-         
-
         </S.WhorksContent>
           </Wrapper>
+      
+         
     </S.Main>
   )
 }
