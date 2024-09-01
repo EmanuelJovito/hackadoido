6fecd239ec7becd9447cb964288e62e9754c578a
Emanuel Jovito <emanuel.jovito@ceuma.br>
2024-09-01 11:47:42 -0300

    feat: finalizando AboutStation

--- src/presentation/modules/AboutStations/AboutStation.styled.ts
+++ src/presentation/modules/AboutStations/AboutStation.styled.ts
@@ -1,5 +1,4 @@
 import { styled } from "styled-components";
-import Image from "next/image";
 
 export const Main = styled.main``
 
@@ -17,11 +16,38 @@ export const TopSection = styled.div`
   margin-bottom: 7rem;
 `
 
+export const ImgCard = styled.div`
+
+`
+
+export const ImageSection = styled.div`
+  height: 20rem;
+ 
+
+
+  
+`
+
 export const ImageTag = styled.div`
   width: 9rem;
   height: 4rem;
   background-color: #0841D3;
-  border-radius: 30px 0 30px 0;
+  border-radius: 30px 0  0 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  bottom: 20px;
+  left: 190px;
+  @media (max-width:768px){
+    left: 190px;
+  }
+  @media (max-width:720px){
+    left: 152px;
+  }
+  @media (max-width:430px){
+    display: none;
+  }
 `
 
 export const ButtonsSection = styled.div`
@@ -42,12 +68,22 @@ export const Button = styled.button`
 
 export const CardsSection = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `
 
 export const CardDescription = styled.div`
   display: flex;
   flex-direction: column;
   padding: 2rem;
+  @media (max-width: 768px){
+    margin-top: 3rem;
+  }
 `
 
 export const Card = styled.div`
@@ -55,24 +91,31 @@ export const Card = styled.div`
   height: 45rem;
   border-radius: 12px;
   box-shadow: 0px 1px 8px 3px #0000002E;
+
+
+  @media (max-width: 768px) {
+    margin-bottom: 5rem;
+    width: 28rem;
+    height: 50rem;
+  }
 `
 
 export const CardTitle = styled.h1`
   color: #0841D3;
   font-size: 2.4rem;
   margin-bottom: 1.5rem;
+  margin-top: 1.5rem;
 `
 
 export const CardSubtitle = styled.p`
   font-size: 1.6rem;
-  margin-bottom: 4rem;
+  margin-bottom: 3.5rem;
 `
 
 export const Locantion = styled.div`
   display: flex;
-  width: 52%;
+  width:280px;
   align-items: center;
-  justify-content: space-between;
   color: #0841D3;
   font-size: 1.5rem;
 `
--- src/presentation/modules/AboutStations/AboutStations.tsx
+++ src/presentation/modules/AboutStations/AboutStations.tsx
@@ -6,6 +6,9 @@ import Image from "next/image";
 import ArrowLeft from '@/../public/ArrowCircleLeft.svg'
 import ArrowRight from '@/../public/ArrowCircleRight.svg'
 import Card1 from '@/../public/card1.jpg'
+import Card2 from '@/../public/card2.jpg'
+import Card3 from '@/../public/card3.jpg'
+import Card4 from '@/../public/card4.jpg' 
 import MapPin from '@/../public/MapPin.svg'
 import Logo from '@/../public/logo.svg'
 
@@ -15,24 +18,63 @@ export function AboutStation() {
       <Wrapper>
         <S.TopSection>
           <S.Title>Conheça as Estações Tech</S.Title>
-          <S.ButtonsSection>
+          {/* <S.ButtonsSection>
             <S.Button>
               <Image src={ArrowLeft} width={50} height={50} alt="Seta para a esquerda"/>
             </S.Button>
             <S.Button>
               <Image src={ArrowRight} width={50} height={50} alt="Seta para a direita"/>
             </S.Button>
-          </S.ButtonsSection>
+          </S.ButtonsSection> */}
         </S.TopSection>
         <S.CardsSection>
           <S.Card>
-            <Image src={Card1} width={280} height={210} alt="Jovens usando um computador"/>
-            <S.ImageTag><Image src={Logo} width={39} height={39} alt="logo"/></S.ImageTag>
+            <S.ImageSection>
+              <S.ImgCard><Image src={Card1}  alt="Jovens usando um computador" style={{borderRadius: '12px 12px 0 0', width: '100%', height: '100%'}}/></S.ImgCard>
+              
+              <S.ImageTag><Image src={Logo} width={39} height={39} alt="logo"/></S.ImageTag>
+            </S.ImageSection>
 
             <S.CardDescription>
               <S.CardTitle>São Vicente Ferrer</S.CardTitle>
               <S.CardSubtitle>Centro de inovação focado em tecnologia e empreendedorismo, oferecendo suporte e capacitação para a comunidade local.</S.CardSubtitle>
               <S.Locantion><Image src={MapPin} width={16} height={16} alt="marcação de mapa"/>R. Dom Pedro II</S.Locantion>
+            </S.CardDescription>
+          </S.Card>
+          <S.Card>
+            <S.ImageSection>
+            <S.ImgCard><Image src={Card3}  alt="Jovens usando um computador" style={{borderRadius: '12px 12px 0 0', width: '100%', height: '100%'}}/></S.ImgCard>
+              <S.ImageTag><Image src={Logo} width={39} height={39} alt="logo"/></S.ImageTag>
+            </S.ImageSection>
+
+            <S.CardDescription>
+              <S.CardTitle>Sol e Mar</S.CardTitle>
+              <S.CardSubtitle>Centro de inovação que promove tecnologia e empreendedorismo, oferecendo recursos e capacitação para a comunidade.</S.CardSubtitle>
+              <S.Locantion><Image src={MapPin} width={16} height={16} alt="marcação de mapa"/>Rua da União - Sol e Mar</S.Locantion>
+            </S.CardDescription>
+          </S.Card>
+          <S.Card>
+            <S.ImageSection>
+            <S.ImgCard><Image src={Card3}  alt="Jovens usando um computador" style={{borderRadius: '12px 12px 0 0', width: '100%', height: '100%'}}/></S.ImgCard>
+              <S.ImageTag><Image src={Logo} width={39} height={39} alt="logo"/></S.ImageTag>
+            </S.ImageSection>
+
+            <S.CardDescription>
+              <S.CardTitle>Maracanã</S.CardTitle>
+              <S.CardSubtitle>Espaço de inovação que oferece capacitação e recursos tecnológicos para a comunidade local.</S.CardSubtitle>
+              <S.Locantion><Image src={MapPin} width={16} height={16} alt="marcação de mapa"/>R. Evandro Bessa - Maracanã</S.Locantion>
+            </S.CardDescription>
+          </S.Card>
+          <S.Card>
+            <S.ImageSection>
+            <S.ImgCard><Image src={Card2}  alt="Jovens usando um computador" style={{borderRadius: '12px 12px 0 0', width: '100%', height: '100%'}}/></S.ImgCard>
+              <S.ImageTag><Image src={Logo} width={39} height={39} alt="logo"/></S.ImageTag>
+            </S.ImageSection>
+
+            <S.CardDescription>
+              <S.CardTitle>João de Deus</S.CardTitle>
+              <S.CardSubtitle>Um espaço dedicado à inovação e inclusão digital, oferecendo cursos e recursos tecnológicos para a comunidade.</S.CardSubtitle>
+              <S.Locantion><Image src={MapPin} width={16} height={16} alt="marcação de mapa"/>Av. Tales Neto - João de Deus</S.Locantion>
             </S.CardDescription>
           </S.Card>
         </S.CardsSection>
