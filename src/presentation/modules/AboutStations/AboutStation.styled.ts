378e403c91e174071870870d4080e0aab6257e5e
Emanuel Jovito <emanuel.jovito@ceuma.br>
2024-09-01 00:48:00 -0300

    WIP: Criando AboutStation

--- src/presentation/modules/AboutStations/AboutStation.styled.ts
+++ src/presentation/modules/AboutStations/AboutStation.styled.ts
@@ -1,17 +1,78 @@
 import { styled } from "styled-components";
 import Image from "next/image";
 
-export const Main = styled.main``;
-
-export const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: black;
-  width: 100wh;
-`;
+export const Main = styled.main``
 
 export const Title = styled.h1`
   font-size: 4.5rem;
-  color: #0841D3
+  color: #0841D3;
+`
+
+export const TopSection = styled.div`
+  width: 100%;
+  height: 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 7rem;
+`
+
+export const ImageTag = styled.div`
+  width: 9rem;
+  height: 4rem;
+  background-color: #0841D3;
+  border-radius: 30px 0 30px 0;
+`
+
+export const ButtonsSection = styled.div`
+  width: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+`
+
+export const CardsSection = styled.div`
+  width: 100%;
+`
+
+export const CardDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+`
+
+export const Card = styled.div`
+  width: 28rem;
+  height: 45rem;
+  border-radius: 12px;
+  box-shadow: 0px 1px 8px 3px #0000002E;
+`
+
+export const CardTitle = styled.h1`
+  color: #0841D3;
+  font-size: 2.4rem;
+  margin-bottom: 1.5rem;
+`
+
+export const CardSubtitle = styled.p`
+  font-size: 1.6rem;
+  margin-bottom: 4rem;
+`
+
+export const Locantion = styled.div`
+  display: flex;
+  width: 52%;
+  align-items: center;
+  justify-content: space-between;
+  color: #0841D3;
+  font-size: 1.5rem;
 `
--- src/presentation/modules/AboutStations/AboutStations.tsx
+++ src/presentation/modules/AboutStations/AboutStations.tsx
@@ -1,12 +1,42 @@
 "use client";
+import { Wrapper } from "@/presentation/shared/components/layout/Wrapper/Wrapper";
 import * as S from "./AboutStation.styled";
 import Mask from "@/../public/Mask group.svg";
-
+import Image from "next/image";
+import ArrowLeft from '@/../public/ArrowCircleLeft.svg'
+import ArrowRight from '@/../public/ArrowCircleRight.svg'
+import Card1 from '@/../public/card1.jpg'
+import MapPin from '@/../public/MapPin.svg'
+import Logo from '@/../public/logo.svg'
 
 export function AboutStation() {
   return (
     <S.Main>
-      <S.Title>Conheça as Estações Tech</S.Title>
+      <Wrapper>
+        <S.TopSection>
+          <S.Title>Conheça as Estações Tech</S.Title>
+          <S.ButtonsSection>
+            <S.Button>
+              <Image src={ArrowLeft} width={50} height={50} alt="Seta para a esquerda"/>
+            </S.Button>
+            <S.Button>
+              <Image src={ArrowRight} width={50} height={50} alt="Seta para a direita"/>
+            </S.Button>
+          </S.ButtonsSection>
+        </S.TopSection>
+        <S.CardsSection>
+          <S.Card>
+            <Image src={Card1} width={280} height={210} alt="Jovens usando um computador"/>
+            <S.ImageTag><Image src={Logo} width={39} height={39} alt="logo"/></S.ImageTag>
+
+            <S.CardDescription>
+              <S.CardTitle>São Vicente Ferrer</S.CardTitle>
+              <S.CardSubtitle>Centro de inovação focado em tecnologia e empreendedorismo, oferecendo suporte e capacitação para a comunidade local.</S.CardSubtitle>
+              <S.Locantion><Image src={MapPin} width={16} height={16} alt="marcação de mapa"/>R. Dom Pedro II</S.Locantion>
+            </S.CardDescription>
+          </S.Card>
+        </S.CardsSection>
+      </Wrapper>
     </S.Main>
   );
 }
